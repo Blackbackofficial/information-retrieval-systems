d1b5a2cad5f50bee09da761c3f3b09cdc9f54883
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2022-03-17 02:48:00 +0300

    Add mergesort && refactoring

--- internal/lab1.go
+++ internal/lab1.go
@@ -9,7 +9,8 @@ import (
 var MIN = -50
 var MAX = 50
 
-func BubbleSort(ar []int) {
+// BubbleSort
+func bubbleSort(ar []int) {
 	var countPerm int
 	var countComp int
 	for i := 0; i < len(ar); i++ {
@@ -29,6 +30,18 @@ func swap(ar []int, i, j int) {
 	tmp := ar[i]
 	ar[i] = ar[j]
 	ar[j] = tmp
+}
+
+// QuickSort
+func quickSort(arr []int, begin, end, countPerm, countComp int) (int, int) {
+	if end-begin < 2 {
+		return countPerm, countComp
+	}
+	index, countPerm, countComp := pivot(arr, begin, end, countPerm, countComp)
+
+	countPerm, countComp = quickSort(arr, begin, index, countPerm, countComp)
+	countPerm, countComp = quickSort(arr, index+1, end, countPerm, countComp)
+	return countPerm, countComp
 }
 
 func pivot(arr []int, begin, end, countPerm, countComp int) (int, int, int) {
@@ -60,17 +73,39 @@ func pivot(arr []int, begin, end, countPerm, countComp int) (int, int, int) {
 	return begin, countPerm, countComp
 }
 
-func RecursiveQuickSort(arr []int, begin, end, countPerm, countComp int) (int, int) {
-	if end-begin < 2 {
-		return countPerm, countComp
+// MergeSort
+func mergeSort(items []int) []int {
+	if len(items) < 2 {
+		return items
 	}
-	index, countPerm, countComp := pivot(arr, begin, end, countPerm, countComp)
-
-	countPerm, countComp = RecursiveQuickSort(arr, begin, index, countPerm, countComp)
-	countPerm, countComp = RecursiveQuickSort(arr, index+1, end, countPerm, countComp)
-	return countPerm, countComp
+	first := mergeSort(items[:len(items)/2])
+	second := mergeSort(items[len(items)/2:])
+	return merge(first, second)
 }
 
+func merge(a []int, b []int) []int {
+	var final []int
+	i := 0
+	j := 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			final = append(final, a[i])
+			i++
+		} else {
+			final = append(final, b[j])
+			j++
+		}
+	}
+	for ; i < len(a); i++ {
+		final = append(final, a[i])
+	}
+	for ; j < len(b); j++ {
+		final = append(final, b[j])
+	}
+	return final
+}
+
+// Lab1 Print output
 func Lab1() {
 	rand.Seed(time.Now().UnixNano())
 	ar10 := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
@@ -83,83 +118,83 @@ func Lab1() {
 
 	fmt.Println("------------ BubbleSort ------------ (count: 10)")
 	fmt.Println("_____SINGLE_____")
-	BubbleSort(ar10)
+	bubbleSort(ar10)
 	Reverse(ar10)
 	fmt.Println("_____REVERSE_____")
-	BubbleSort(ar10)
+	bubbleSort(ar10)
 	fmt.Println("_____RANDOM_____")
 
 	arr10 := make([]int, 10)
 	copy(arr10, ranAr10)
-	BubbleSort(arr10)
+	bubbleSort(arr10)
 
 	fmt.Println("------------ BubbleSort ------------ (count: 20)")
 	fmt.Println("_____SINGLE_____")
-	BubbleSort(ar20)
+	bubbleSort(ar20)
 	Reverse(ar20)
 	fmt.Println("_____REVERSE_____")
-	BubbleSort(ar20)
+	bubbleSort(ar20)
 	fmt.Println("_____RANDOM_____")
 	arr20 := make([]int, 20)
 	copy(arr20, ranAr20)
-	BubbleSort(arr20)
+	bubbleSort(arr20)
 
 	fmt.Println("------------ BubbleSort ------------ (count: 40)")
 	fmt.Println("_____SINGLE_____")
-	BubbleSort(ar40)
+	bubbleSort(ar40)
 	Reverse(ar40)
 	fmt.Println("_____REVERSE_____")
-	BubbleSort(ar40)
+	bubbleSort(ar40)
 	fmt.Println("_____RANDOM_____")
 	arr40 := make([]int, 40)
 	copy(arr40, ranAr40)
-	BubbleSort(arr40)
+	bubbleSort(arr40)
 
 	fmt.Println("------------ QuickSort ------------ (count: 10)")
 	fmt.Println("_____SINGLE_____")
-	countPerm, countComp := RecursiveQuickSort(ar10, 0, len(ar10), 0, 0)
+	countPerm, countComp := quickSort(ar10, 0, len(ar10), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 	Reverse(ar10)
 	fmt.Println("_____REVERSE_____")
-	countPerm, countComp = RecursiveQuickSort(ar10, 0, len(ar10), 0, 0)
+	countPerm, countComp = quickSort(ar10, 0, len(ar10), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 	fmt.Println("_____RANDOM_____")
 	copy(arr10, ranAr10)
-	countPerm, countComp = RecursiveQuickSort(arr10, 0, len(arr10), 0, 0)
+	countPerm, countComp = quickSort(arr10, 0, len(arr10), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 
 	fmt.Println("\n\n------------ QuickSort ------------ (count: 20)")
 	fmt.Println("_____SINGLE_____")
-	countPerm, countComp = RecursiveQuickSort(ar20, 0, len(ar20), 0, 0)
+	countPerm, countComp = quickSort(ar20, 0, len(ar20), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 	Reverse(ar20)
 	fmt.Println("_____REVERSE_____")
-	countPerm, countComp = RecursiveQuickSort(ar20, 0, len(ar20), 0, 0)
+	countPerm, countComp = quickSort(ar20, 0, len(ar20), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 	fmt.Println("_____RANDOM_____")
 	copy(arr20, ranAr20)
-	countPerm, countComp = RecursiveQuickSort(arr20, 0, len(arr20), 0, 0)
+	countPerm, countComp = quickSort(arr20, 0, len(arr20), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 
 	fmt.Println("\n\n------------ QuickSort ------------ (count: 40)")
 	fmt.Println("_____SINGLE_____")
-	countPerm, countComp = RecursiveQuickSort(ar40, 0, len(ar40), 0, 0)
+	countPerm, countComp = quickSort(ar40, 0, len(ar40), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 	Reverse(ar40)
 	fmt.Println("_____REVERSE_____")
-	countPerm, countComp = RecursiveQuickSort(ar40, 0, len(ar40), 0, 0)
+	countPerm, countComp = quickSort(ar40, 0, len(ar40), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 	fmt.Println("_____RANDOM_____")
 	copy(arr40, ranAr40)
-	countPerm, countComp = RecursiveQuickSort(arr40, 0, len(arr40), 0, 0)
+	countPerm, countComp = quickSort(arr40, 0, len(arr40), 0, 0)
 	fmt.Printf("Permutations: %d\n", countPerm)
 	fmt.Printf("Сomparison: %d\n", countComp)
 
