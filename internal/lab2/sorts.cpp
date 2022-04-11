6e9c3a56485eeb5d75d3c0cd15e110b55cf11c84
Blackbackofficial <Chernov-Ivan.1997@yandex.ru>
2022-04-11 04:40:35 +0300

    Add merge sort on c++

--- internal/lab2/sorts.cpp
+++ internal/lab2/sorts.cpp
@@ -6,6 +6,71 @@ using namespace std;
 
 static int NUM_OF_TAPES = 4;
 
+// MergeSort
+void merge(int array[], int const left, int const mid, int const right) {
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+    auto *leftArray = new int[subArrayOne], *rightArray = new int[subArrayTwo];
+
+    // Copy data to temp arrays leftArray[] and rightArray[]
+    for (auto i = 0; i < subArrayOne; i++) {
+        leftArray[i] = array[left + i];
+    }
+    for (auto j = 0; j < subArrayTwo; j++) {
+        rightArray[j] = array[mid + 1 + j];
+    }
+
+    auto indexOfSubArrayOne = 0, // Initial index of first sub-array
+    indexOfSubArrayTwo = 0; // Initial index of second sub-array
+    int indexOfMergedArray = left; // Initial index of merged array
+
+    // Merge the temp arrays back into array[left..right]
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        } else {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // left[], if there are any
+    while (indexOfSubArrayOne < subArrayOne) {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+    // Copy the remaining elements of
+    // right[], if there are any
+    while (indexOfSubArrayTwo < subArrayTwo) {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
+}
+
+void mergeSort(int array[], int const begin, int const end) {
+    if (begin >= end) {
+        return;
+    }
+
+    auto mid = begin + (end - begin)/2;
+    mergeSort(array, begin, mid);
+    mergeSort(array, mid + 1, end);
+    merge(array, begin, mid, end);
+}
+
+void printArray(int A[], int size) {
+    for (auto i = 0; i < size; i++) {
+        cout << A[i] << " ";
+    }
+}
+
+
+// Oscillated Sorting
 class Tape {
 public:
     vector<vector<int>> vData;
@@ -82,6 +147,20 @@ vector<int> merge(vector<int> a, vector<int> b) {
 }
 
 int main() {
+    cout << "------------ Balanced MergeSort ------------ (count: 20)\n";
+    int arr[] = { 54, 26, 93, 17, 77, 31, 44, 55, 20, 40, 20, 55, 1, 7, 39, 11, 25, 19, 34, 10 };
+    auto arr_size = sizeof(arr)/sizeof(arr[0]);
+
+    cout << "Given array is \n";
+    printArray(arr, arr_size);
+
+    mergeSort(arr, 0, arr_size - 1);
+
+    cout << "\nSorted array is \n";
+    printArray(arr, arr_size);
+
+
+    cout << "\n\n------------ Oscillated Sorting (MergeSort) ------------ (count: 20)\n";
     vector<Tape> tapes;
 
     vector<int> data;
